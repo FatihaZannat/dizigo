d8f7e126045e54ba0e33392039ea07c523a2008f
Fatiha Zannat <fatihazannat.n@gmail.com>
2024-05-17 21:59:26 +0600

    preloader & sticky header

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,11 +4,22 @@
         return $(selector).length > 0
     } 
 
+    $(window).on('load', function(){
+        preloader()
+    })
 
     // ready function
     $(function(){
         dynamicBackground()
+        stickyHeader()
     })
+
+    // preloader--------------
+
+    function preloader() {
+        $('.fz_preloader_in').fadeOut();
+        $('.fz_preloader').delay(150).fadeOut('slow')
+    }
 
 
     // --------dynamicBackground
@@ -21,4 +32,37 @@
             })
         })
     }
+
+    // -----------stickyHeader
+    function stickyHeader(){
+        let $window = $(window)
+        let lastScrollTop = 0;
+        let $header = $('.fz_sticky_header')
+        let headerHeight = $header.outerHeight() + 30
+
+        $window.scroll(function(){
+          let windowTop = $window.scrollTop()
+         
+
+          if(windowTop >= headerHeight) {
+            $header.addClass('fz_gescout_sticky')
+            //   console.log('windotop , headerscroll', windowTop , lastScrollTop);
+          } else{
+            // console.log('windotop , headerscroll else', windowTop , lastScrollTop);
+            $header.removeClass('fz_gescout_sticky')
+            $header.removeClass('fz_gescout_show')
+          }
+        if($header.hasClass('fz_gescout_sticky')){
+            if(windowTop < lastScrollTop){
+                $header.addClass('fz_gescout_show')
+            }else{
+                // console.log('lassss');
+                $header.removeClass('fz_gescout_show')
+            }
+        }
+        lastScrollTop = windowTop
+        })
+    }
+
+
 })(jQuery)
